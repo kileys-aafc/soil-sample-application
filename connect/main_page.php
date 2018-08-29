adcdc86462c88630007e995a8d816accdfbadace
kileys-aafc <39916018+kileys-aafc@users.noreply.github.com>
2018-08-29 14:03:34 -0400

    updated main page layout

--- connect/main_page.php
+++ connect/main_page.php
@@ -1,58 +1,47 @@
-<html>
-     <?php
-        include 'index.php';
-        ?>    
-  
-<head>
-    
-    <title>Soil Sample Management</title>
-        
-    <script type="text/javascript">
-     
-        function hideFunc(Content){ 
-            var x=document.getElementById(Content);
-            if(x.style.display==="none"){
-                x.style.display="block";
-            }else{
-                x.style.display="none";
-            }
-        }    
-    </script>    
-</head>
-    
-    
-<body>
-    
+<?php include 'index.php';?>    
 
-<div class="homerow">
-    <p class="big-1" style="padding-left:80px;">Welcome <?php   echo htmlspecialchars($_SESSION['username']);   ?>. </p>    
-    <div class = "homepage homeleft">   
-        
-        <p><img src="../connect/images/SitesLocationP4.png" width="1000" height="500" alt="site image"></p>  
-    </div>
-    <div class="homepage homeright">
-        <!---
-	<button class="homebutton" onclick="hideFunc('hideIns')">Instruction</button>
-        <div id = "hideIns">
-            <p>About this System</p>
-            <p>This soil management system is an integrated system that users can add, update and query the soil sample data in the database.</p>
-        </div>  
-        
-        <button class="homebutton" onclick="hideFunc('hideIns2')">About</button>
-        <div id = "hideIns2">
-            <p>About this System</p>
-            <p>This soil management system is an integrated system that users can add, update and query the soil sample data in the database</p>
-            <p>This soil management system is an integrated system that users can add, update and query the soil sample data in the database</p>
+<main role="main" class="container">
+
+<section class="jumbotron text-center">
+        <div class="container">
+          <h1 class="jumbotron-heading">CanSIS Soil Samples</h1>
+          <p class="lead text-muted">This application allows you to query and manage the contents of the CanSIS Soil Sample Collection.</p>
+          <!-- <p>
+            Button to click <a href="#" class="btn btn-primary my-2">Main call to action</a> 
+          </p>
+          -->
         </div>
-	--->
+</section>
+
+<div class="card-deck">
+  <div class="card">    
+        <i class="fas fa-search bg-secondary text-center text-light py-4" style="font-size: 1000%"></i>
+  <div class="card-body">
+      <h5 class="card-title">Search Database</h5>
+      <p class="card-text">Query soil samples currently stored in the database.</p>
+      <p><a class="btn btn-secondary" href="http://<?php echo $host ?>/soil/connect/query/querySample.php" role="button">View details</a></p>
     </div>
-    
-     
+  </div>
+  <div class="card">
+  <i class="fas fa-qrcode bg-secondary text-center text-light py-4" style="font-size: 1000%"></i>
+    <div class="card-body">
+      <h5 class="card-title">Print Labels</h5>
+      <p class="card-text">Print labels for storing soil samples in the warehouse.</p>
+      <p><a class="btn btn-secondary" href="http://<?php echo $host ?>/soil/connect/label/create_label.php" role="button">View details</a></p>
+    </div>
+  </div>
+  <div class="card">
+  <i class="far fa-question-circle bg-secondary text-center text-light py-4" style="font-size: 1000%"></i>
+    <div class="card-body">
+      <h5 class="card-title">About the System</h5>
+      <p class="card-text">Information about the database and warehouse storage.</p>
+      <p><a class="btn btn-secondary" href="http://<?php echo $host ?>/soil/connect/about/about.php" role="button">View details</a></p>
+    </div>
+  </div>
 </div>
-    
-
- 
 
 
+   
+</main>
 </body>    
 </html>
