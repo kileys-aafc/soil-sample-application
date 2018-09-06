920e554704a2af1179b463b78083689cd9e345f4
kileys-aafc <39916018+kileys-aafc@users.noreply.github.com>
2018-09-06 11:38:45 -0400

    updated layout

--- connect/about/contact.php
+++ connect/about/contact.php
@@ -1,34 +1,13 @@
-<?php
- include '../index.php';  
-?>
-<html>
-    <head>
-        <title>Contact</title>
-
-    </head>
-    
-    <body>
-        <div class="page-subtitle">
-        <p class="big-2" style="width:100%;"><strong>Contact Us</strong></p>
+<head><title>Contact Admin</title></head>
+<?php include '../index.php';?>    
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1 class="display-4 mb-4">Contact Us</h1>
+            <p class="pl-2">If you require assistance, have questions, or find any bugs, email the administrator</p>
+            <a class="pl-2" href="mailto:simon.kiley@agr.gc.ca">Contact Admin</a></p>
         </div>
-        
-        <div class="page-main-content">    
-            <div id="Contact_send_email">
-                <p>Canadian Soil Information Service (CanSIS)</p>
-                <p>Room 1135, K.W. Neatby Building</p>
-                <p>960 Carling Ave.</p>
-                <p>Ottawa, Ontario, Canada</p>
-                <p>K1A 0C6</p>
-                <P>Email:
-                    <a href="mailto:infocansis@agr.gc.ca"><u>infocansis@agr.gc.ca</u></a>
-                </P>
-                
-                
-                
-            </div>
-        </div>  
-    </body>
-           
-    
-    
+    </div>
+</div>
+</body>
 </html>
--- connect/about/data_structure.php
+++ connect/about/data_structure.php
@@ -1,37 +1,22 @@
-<html>
-    <?php
-        include '../index.php';  
-    ?>
-
 <head><title>Data Structure</title></head>  
-
-<body>
-    <div  class="page-subtitle">
-       <!--- <p class="big-1">Add a New Sample</p>--->
-        <p class="big-2"><strong>Database Structure</strong></p>
-    </div>
-   
-    <div class="page-main-content">
-         <div class = "data_structure dataStrleft">   
-            <img id = "dataStrImg" 
-                 src = "../images/databaseStructure.png" 
-                 alt="Database Structure Explanation" 
-                 align="bottom"
-                 width="100%"><br>
-            
+<?php include '../index.php'; ?>
+<div class="container">
+    <div class="row mb-4">
+        <div class="col">
+            <h1 class="display-4">Database Structure</h1>
         </div>
-        <div class = "data_structure dataStrright">   
-             <p>
-            In this system, all soil sample data are stored in multiple tables in a MySQL database with the name of "soiltest1".
-            </p>
-            <p>
-            All tables are connected with the general tables by having a field that refers the same information in the general table. for example, the "site_info" table has site number field which also exist in "sample" table.
-            </p>
-            <br>
-            <p>If more information need to be included in this database, please contact the administrator to modify the database by adding new table or new field. </p>        </div>
-        
     </div>
-
+    <div class="row">
+        <div class="col-10 ml-1">
+            <p>In this system soil sample data is stored in multiple tables in a MySQL database</p>
+            <p>The data model is shown below:</p>
+        </div>
+    </div>
+    <div class="row my-3">
+        <div class="col">   
+            <img class="img-fluid" src="../images/databaseStructure.png" alt="Data Model">
+        </div>
+    </div>
+</div>
 </body>
-    
 </html>
