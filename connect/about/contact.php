814f5fcd0c1ac48cd64ca8b4646a1d22ef1cc8e3
kileys-aafc <39916018+kileys-aafc@users.noreply.github.com>
2018-09-10 14:48:43 -0400

    updated layouts

--- connect/about/contact.php
+++ connect/about/contact.php
@@ -4,8 +4,8 @@
     <div class="row justify-content-center">
         <div class="col-8">
             <h1 class="display-4 mb-4 text-center">Contact Us</h1>
-            <p class="pl-2">If you require assistance, have questions, or find any bugs, email the administrator</p>
-            <a class="pl-2" href="mailto:simon.kiley@agr.gc.ca">Contact Admin</a></p>
+            <p class="text-center">If you require assistance, have questions, or find any bugs, email the administrator</p>
+            <p class="text-center"><a href="mailto:simon.kiley@agr.gc.ca">Contact Admin</a></p>
         </div>
     </div>
 </div>
