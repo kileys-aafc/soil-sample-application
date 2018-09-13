1daf49e855479bb6d419a676501e34984e5d1190
kileys-aafc <39916018+kileys-aafc@users.noreply.github.com>
2018-09-13 13:06:55 -0400

    updated layout

--- connect/add/addSite.php
+++ connect/add/addSite.php
@@ -1,115 +1,87 @@
-<html>
-<?php
- include '../index.php';  ?>
- <head><title>Add a New Site</title></head>   
+<head><title>Add a New Site</title></head> 
+<?php include '../index.php'; ?>
 
-<body>
-    <div id = "add-site">
-    
-    <p class="big-2"><strong>Add a New Site</strong></p>  
-   
-  
-    
-    
-        <form action="siteAdded.php"    method="post">
-            <table >
-                <col width="300px">
-                <col width="100px">
-                <col width="100px">
-                <col width="100px">
-                <tr>
-                    <td>Site Number:</td>
-                    <td colspan="3"> <input required type ="text" name="site_num"  maxlength="2" value = "" /></td>
-                </tr>
+<div class="container" id="add-site">
+    <h1 class="display-4 text-center mb-4">Add a New Site</h1>
+    <div class="row justify-content-center">
+        <div class="col-sm-5 mt-4">
+            <form action="siteAdded.php" method="post">
+                <div class="form-group row">
+                    <label class="col-sm-4 col-form-label" for="site_num">Site Number</label>
+                    <div class="col-sm-8">
+                        <input class="form-control" required type="text" name="site_num"  maxlength="2" value="" />
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 col-form-label" for="site_prov">Site Province</label>
+                    <div class="col-sm-8"> 
+                        <input class="form-control" required type="text" name="site_prov" maxlength="2" value = "" />
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 col-form-label" for="site_name">Site Name</label>
+                    <div class="col-sm-8">
+                        <input class="form-control" required type ="text" name="site_name"  value = "" />
+                    </div>
+                </div>
+                <div class="form-group row no-gutters">
+                    <label class="col-sm-4 col-form-label mr-2" for="lat">Site Latitude</label>
+                    <div class="col-sm">
+                        <input  class=" form-control form-control-sm" id="lat" type="text"  name="lat_d" maxlength="2"  value="" placeholder="Degrees" />
+                    </div>
+                    <div class="col-sm">
+                        <input  class="form-control form-control-sm" type="text"  name="lat_m" maxlength="2"  value="" placeholder="Minutes" />
+                    </div>
+                    <div class="col-sm">
+                        <input  class="form-control form-control-sm" type="text"  name="lat_s" maxlength="2"  value="" placeholder="Seconds" />
+                    </div>
+                </div>
+                <div class="form-group row row no-gutters">
+                    <label class="col-sm-4 col-form-label mr-2" for="long">Site Longitude</label>
+                    <div class="col-sm">
+                        <input  class="form-control form-control-sm" id="long" type="text"  name="lon_d" maxlength="2"  value="" placeholder="Degrees" />
+                    </div>
+                    <div class="col-sm">
+                        <input  class="form-control form-control-sm" type ="text"  name="lon_m" maxlength="2"  value = "" placeholder="Minutes" />
+                    </div>
+                    <div class="col-sm">
+                        <input  class="form-control form-control-sm" type ="text"  name="lon_s" maxlength="3"  value = "" placeholder="Seconds" />
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 col-form-label" for="size_ha">Site Size (ha)</label>
+                    <div class="col-sm">
+                        <input class="form-control" required type="text" name="size_ha" maxlength="50" value = "" />
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 col-form-label" for="year_established">Year Established</label>
+                    <div class="col-sm">
+                        <select class="form-control" required name="year_established">
+                            <option value="" placeholder="Please Select" hidden></option>
+                                <?php
 
-                <tr>
-                    <td>Site Province:</td>
-                    <td colspan="3"> <input required type ="text" name="site_prov" maxlength="2" value = "" /></td>
-                </tr>
-
-                <tr>
-                    <td>Site Name:</td>
-                    <td colspan="3"> <input required type ="text" name="site_name"  value = "" /></td>
-                </tr>
-
-                  <tr>
-                    <td>Site Latitude [N]</td>
-                    <td><input  type ="text"  name="lat_d" maxlength="2"  value = "" placeholder="degree" /> </td>
-                    <td><input  type ="text"  name="lat_m" maxlength="2"  value = "" placeholder="minute" /> </td>
-                    <td><input  type ="text"  name="lat_s" maxlength="2"  value = "" placeholder="second" /> </td>
-                </tr>
-
-                <tr>
-                    <td>Site Longitude [W]</td>
-                    <td><input  type ="text"  name="lon_d" maxlength="2"  value = "" placeholder="degree" /> </td>
-                    <td><input  type ="text"  name="lon_m" maxlength="2"  value = "" placeholder="minute" /> </td>
-                    <td><input  type ="text"  name="lon_s" maxlength="3"  value = "" placeholder="second" /> </td>
-                </tr>
-
-                <tr>
-                    <td>Site Size (in ha):</td>
-                    <td colspan="3"> <input required type ="text" name="size_ha" maxlength="50" value = "" /></td>
-                </tr>
-
-                <tr>
-                    <td>Year Established(YYYY):</td>
-                    <td colspan="3"> <select required name="year_establish" style="width:100%; height:28pt;">
-                    <option value="">-Please Select-</option>
-                <?php
-
-                for ($i=2000;$i>=1980;$i--){
-                    ?>
-                <option value="<?php echo $i;?>"><?php echo $i;?></option>
-                <?php
-                }
-                ?>
-                    </select></td>
-                </tr>
-
-                <tr>
-                    <td>Ecological Setting:</td>
-                    <td colspan="3"><input required type ="text" name="ecol_setting" maxlength="50" value = "" /> 
-                </tr>
-
-
-            </table> 
-
-            <p style="width:600px;">
-                <input type = "submit" value = "Add this New Site"/>
-            </p>
-            
-        </form>
+                                for ($i=2000;$i>=1980;$i--){
+                                    ?>
+                                <option value="<?php echo $i;?>"><?php echo $i;?></option>
+                                <?php
+                                }
+                                ?>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 col-form-label" for="ecol_setting">Ecological Setting</label>
+                    <div class="col-sm">
+                        <input class="form-control" required type="text" name="ecol_setting" maxlength="50" value = "" />
+                    </div>
+                </div>
+                <div class="text-center"> 
+                    <input class="btn btn-primary" type="submit" value="Add New Site"/>
+                </div>
+            </form>
         
         
 </div>
 </body>
-<style>
-   
-
-  
-    input[type = text]{
-        height:28pt;
-        width: 100%;
-    }
-
-    td{
-       
-        padding: 3px 2px;
-    }
-    
-    table{
-        font-size: 13pt;
-    }
-    #add-site{
-        padding-left: 50px;
-        line-height: 2.5; 
-        width: 60%;
-        float: left;
-        margin-left: 50px;
-  
-    }
-    
-
-   
-</style>
 </html>
