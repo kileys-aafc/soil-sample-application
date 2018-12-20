cea0d1eab7ee90a12eedbe2874d6d8c2936c33f9
kileys-aafc <simon.kiley@canada.ca>
2018-12-20 13:42:33 -0500

    title change

--- login.php
+++ login.php
@@ -74,7 +74,7 @@
     
     <link rel="icon" href="images/soil-filled.png">
 
-    <title>Soil Sample Signin</title>
+    <title>CanSIS Archive Sign In</title>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
@@ -88,7 +88,7 @@
   
     <form class="form-signin" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
       <img class="mb-4" src="images/soil.png" alt="" width="80" height="80">       
-      <h1 class="h3 mb-3 font-weight-normal">Soil Sample Database</h1>
+      <h1 class="h3 mb-3 font-weight-normal">CanSIS Sample Archive</h1>
 
       <label for="inputUser" class="sr-only" >Username</label>
       <input id="inputUser" name="username" class="form-control" placeholder="Username" required=""  value="<?php echo $username;?>" autofocus="" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;" autocomplete="off" type="text">
