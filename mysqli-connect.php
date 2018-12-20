24eced2bb93cb4df17d1a70ae65af7d53a66e2f4
kileys-aafc <simon.kiley@canada.ca>
2018-12-20 14:10:54 -0500

    updated db name

--- mysqli-connect.php
+++ mysqli-connect.php
@@ -2,7 +2,7 @@
     DEFINE('DB_USER','phpmyadmin'); //change as required 
     DEFINE ('DB_PASSWORD','soil'); //change as required
     DEFINE('DB_HOST','localhost'); //change as required
-    DEFINE('DB_NAME','CHENWEN'); //change as required
+    DEFINE('DB_NAME','soil_samples'); //change as required
 
 
     $dbc=@mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) OR die('Could not connect to MySQL'. mysqli_connect_error());
