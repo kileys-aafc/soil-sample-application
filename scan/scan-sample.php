3c930ca905008b1c5dec0d5a59c6fb0fdd836418
kileys-aafc <simon.kiley@canada.ca>
2019-01-29 16:08:21 -0500

    minor layout change

--- scan/scan-sample.php
+++ scan/scan-sample.php
@@ -1,17 +1,17 @@
 <head><title>Scan Label</title></head>
 <?php include '../nav-template.php';?>
 <div class="container">
-    <div class="row mb-5">
+    <div class="row mb-3">
         <div class="col">
             <h1 class="display-4 text-center">Scan Label</h1>
         </div>
     </div>
-    <div class='row justify-content-center'>
-        <p class="text-muted pb-2 ">To query sample data, scan label QR code</p>
+    <div class='row justify-content-center mb-4'>
+        <p class="text-muted">To query sample data, scan QR code</p>
     </div>
     <div class="row justify-content-center">
         <div class="col-4">            
-            <form action="scan-results.php" method="post"> 
+            <form action="scan-results" method="post"> 
                 <div id="create-label-query" class="form-group">
                     <label for="sample-id">Sample ID</label>  
                     <input required class="form-control" type="text" name="sample-id-value" placeholder="Enter Sample ID" autofocus>
