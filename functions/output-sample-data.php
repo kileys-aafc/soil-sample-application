e479a93f16e120b38cac0a1ec13c5906a4f1bc25
kileys-aafc <simon.kiley@canada.ca>
2019-02-11 16:00:37 -0500

    new output layout

--- functions/output-sample-data.php
+++ functions/output-sample-data.php
@@ -1,218 +1,260 @@
-<?php
-/*---------------------------------------------------------------------------------------------------
-this file is used to illustrate the informations of a single sample 
-after it is updated or after that sample is added
----------------------------------------------------------------------------------------------------
-files include this php file are:
-    ../update/update3.php
-    ../add/sample_added.php
------------------------------------------------------------------------------------------------------*/
-echo'
-<div class="col-md">
-    <p><strong>General Info</strong></p>
-    <table class="table table-secondary table-striped table-bordered table-sm">
-        <tbody>
-            <tr>
-                <th>Sample ID</th>
-                <td>'.$sample_id.'</td>
-            </tr>
-            <tr>
-                <th scope="row">Site Number</th>
-                <td>'.$site_num.'</td>
-            </tr>
-            <tr>
-                <th scope="row">Field ID</th>
-                <td>'.$field_id.'</td>
-            </tr>
-            <tr>
-                <th scope="row">Site Type</th>
-                <td>'.$site_type.'</td>
-            </tr>
-            <tr>
-                <th scope="row">Year</th>
-                <td>'.$year.'</td>
-            </tr>
-            <tr>
-                <th scope="row">Sample Number</th>
-                <td>'.$sample_num.'</td>
-            </tr>
-            <tr>
-                <th scope="row">Lab NO.</th>
-                <td>'.$lab_num.'</td>
-            </tr>
-            <tr>
-                <th scope="row">Zone Number</th>
-                <td>'.$zone.'</td>
-            </tr>
-            <tr>
-                <th scope="row">Shelf Number</th>
-                <td>'.$shelf.'</td>
-            </tr>
-            <tr>
-                <th scope="row">Level in Shelf</th>
-                <td>'.$level.'</td>
-            </tr>
-            <tr>
-                <th scope="row">Row in Level</th>
-                <td>'.$rowrow.'</td>
-            </tr>
-            <tr>
-                <th scope="row">Box in the Row</th>
-                <td>'.$box.'</td>
-            </tr>
-        </tbody>
-    </table>
-</div>';
-
-
-echo'
-<div class="col-md">
-<p><strong>Physical Info</strong></p>
-<table class="table table-secondary table-striped table-bordered table-sm">
-    <tbody>
-        <tr>
-            <th>Lab</th>
-            <td>'.$LAB.'</td>
-        </tr>
-        <tr>
-            <th scope="row">Location</th>
-            <td>'.$LOCATION.'</td>
-        </tr>
-        <tr>
-            <th scope="row">Depth</th>
-            <td>'.$DEPTH.'</td>
-        </tr>
-        <tr>
-            <th scope="row">Sand</th>
-            <td>'.$SAND.'</td>
-        </tr>
-        <tr>
-            <th scope="row">Clay</th>
-            <td>'.$CLAY.'</td>
-        </tr>
-        <tr>
-            <th scope="row">Silt</th>
-            <td>'.$SILT.'</td>
-        </tr>
-        <tr>
-            <th scope="row">Sand_VC</th>
-            <td>'.$SAND_VC.'</td>
-        </tr>
-        <tr>
-            <th scope="row">Sand_C</th>
-            <td>'.$SAND_C.'</td>
-        </tr>
-        <tr>
-            <th scope="row">Sand_M</th>
-            <td>'.$SAND_M.'</td>
-        </tr>
-        <tr>
-            <th scope="row">Sand_F</th>
-            <td>'.$SAND_F.'</td>
-        </tr>
-        <tr>
-            <th scope="row">Sand_VF</th>
-            <td>'.$SAND_VF.'</td>
-        </tr>
-    </tbody>
-</table>
+<?php echo'
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <h4><strong>Sample Info</strong></h4>
+            <table class="table table-secondary table-striped table-bordered table-sm">
+                <tbody>
+                <tr>
+                    <th>Sample ID</th>
+                    <td>'.$sample_id.'</td>
+                </tr>
+                <tr>
+                    <th scope="row">Location ID</th>
+                    <td>'.$loc_id.'</td>
+                </tr>
+                <tr>
+                    <th scope="row">Project ID</th>
+                    <td>'.$proj_id.'</td>
+                </tr>
+                <tr>
+                    <th scope="row">Year</th>
+                    <td>'.$year.'</td>
+                </tr>
+                <tr>
+                    <th scope="row">Date</th>
+                    <td>'.$date.'</td>
+                </tr>
+                <tr>
+                    <th scope="row">Province</th>
+                    <td>'.$prov.'</td>
+                </tr>
+                <tr>
+                    <th scope="row">Upper Depth</th>
+                    <td>'.$u_depth.'</td>
+                </tr>
+                <tr>
+                    <th scope="row">Lower Depth</th>
+                    <td>'.$l_depth.'</td>
+                </tr>
+                <tr>
+                    <th scope="row">Horizon</th>
+                    <td>'.$horizon.'</td>
+                </tr>
+                <tr>
+                    <th scope="row">Original ID</th>
+                    <td>'.$orig_id.'</td>
+                </tr>
+                <tr>
+                    <th scope="row">Notes</th>
+                    <td>'.$notes.'</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-md-4">
+            <h4><strong>Physical</strong></h4>
+            <table class="table table-secondary table-striped table-bordered table-sm">
+                <tbody>
+                    <tr>
+                        <th>Sample ID</th>
+                        <td>'.$sample_id.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Bulk Density</th>
+                        <td>'.$bulkd.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Total Gravel %</th>
+                        <td>'.$t_gravel.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Total Clay %</th>
+                        <td>'.$t_clay.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Total Silt %</th>
+                        <td>'.$t_silt.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Total Sand %</th>
+                        <td>'.$t_sand.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Very Coarse Sand</th>
+                        <td>'.$vc_sand.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Coarse Sand</th>
+                        <td>'.$c_sand.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Medium Sand</th>
+                        <td>'.$m_sand.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Fine Sand</th>
+                        <td>'.$f_sand.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Very Fine Sand</th>
+                        <td>'.$vf_sand.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Soil texture</th>
+                        <td>'.$texture.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Field Soil Texture</th>
+                        <td>'.$field_txt.'</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-md-4">
+            <h4><strong>Chemical</strong></h4>
+            <table class="table table-secondary table-striped table-bordered table-sm">
+                <tbody>
+                    <tr>
+                        <th>Sample ID</th>
+                        <td>'.$sample_id.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">pH (CaCl<sub>2</sub>)</th>
+                        <td>'.$ph_cacl2.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">pH (H<sub>2</sub>O)</th>
+                        <td>'.$ph_h2o.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Total Carbon %</th>
+                        <td>'.$ttl_c.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Total Nitrogen %</th>
+                        <td>'.$ttl_n.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">CaCO<sub>3</sub></th>
+                        <td>'.$caco3.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Organic Carbon %</th>
+                        <td>'.$org_c.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Organic Carbon (Non-Calcareous)</th>
+                        <td>'.$org_c_n.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Total Exchangable Cations</th>
+                        <td>'.$tec.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Cation Exchange Capacity</th>
+                        <td>'.$cec.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Exchangable Ca</th>
+                        <td>'.$ca_exch.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Exchangable Mg</th>
+                        <td>'.$mg_exch.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Exchangable K</th>
+                        <td>'.$k_exch.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Exchangable Na</th>
+                        <td>'.$na_exch.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Available P (Î¼g/g); NaHCO<sub>3</sub> extractable</th>
+                        <td>'.$avail_pbi.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Available P (Î¼g/g); Bray Method</th>
+                        <td>'.$avail_pbr.'</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <h4><strong>Archive Jar #1</strong></h4>
+            <table class="table table-secondary table-striped table-bordered table-sm">
+                <tbody>
+                    <tr>
+                        <th>Sample ID</th>
+                        <td>'.$sample_id.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Jar Number</th>
+                        <td>'.$jar_1.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Year Archived</th>
+                        <td>'.$arch_year_jar_1.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Section</th>
+                        <td>'.$section_jar_1.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Column</th>
+                        <td>'.$column_jar_1.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Row</th>
+                        <td>'.$row_jar_1.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Box ID</th>
+                        <td>'.$box_id_jar_1.'</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-md-4">
+            <h4><strong>Archive Jar #2</strong></h4>
+            <table class="table table-secondary table-striped table-bordered table-sm">
+                <tbody>
+                    <tr>
+                        <th>Sample ID</th>
+                        <td>'.$sample_id.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Jar Number</th>
+                        <td>'.$jar_2.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Year Archived</th>
+                        <td>'.$arch_year_jar_2.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Section</th>
+                        <td>'.$section_jar_2.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Column</th>
+                        <td>'.$column_jar_2.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Row</th>
+                        <td>'.$row_jar_2.'</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Box ID</th>
+                        <td>'.$box_id_jar_2.'</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
-';
 
-echo'
-<div class="col-md">
-<p><strong>Chemical Info</strong></p>
-<table class="table table-secondary table-striped table-bordered table-sm">
-    <tbody>
-        <tr>
-            <th>ORG_MTR</th>
-            <td>'.$ORG_MTR.'</td>
-        </tr>
-        <tr>
-            <th scope="row">CEC</th>
-            <td>'.$CEC.'</td>
-        </tr>
-        <tr>
-            <th scope="row">BUFFER_PH</th>
-            <td>'.$BUFFER_PH.'</td>
-        </tr>
-        <tr>
-            <th scope="row">PER_K</th>
-            <td>'.$PER_K.'</td>
-        </tr>
-        <tr>
-            <th scope="row">PER_MG</th>
-            <td>'.$PER_MG.'</td>
-        </tr>
-        <tr>
-            <th scope="row">PER_CA</th>
-            <td>'.$PER_CA.'</td>
-        </tr>
-        <tr>
-            <th scope="row">PER_NA</th>
-            <td>'.$PER_NA.'</td>
-        </tr>
-    </tbody>
-</table>
-</div>
-</div>
-';
-
-echo'
-<div class="row">
-<div class="col-sm-4">
-<p><strong>Soil-Biome Info</strong></p>
-<table class="table table-secondary table-striped table-bordered table-sm">
-    <tbody>
-        <tr>
-            <th>Biome01</th>
-            <td>'.$biome01.'</td>
-        </tr>
-        <tr>
-            <th scope="row">Biome02</th>
-            <td>'.$biome02.'</td>
-        </tr>
-        <tr>
-            <th scope="row">Biome03</th>
-            <td>'.$biome03.'</td>
-        </tr>
-        <tr>
-            <th scope="row">Biome04</th>
-            <td>'.$biome04.'</td>
-        </tr>
-        <tr>
-            <th scope="row">Biome05</th>
-            <td>'.$biome05.'</td>
-        </tr>
-        <tr>
-            <th scope="row">Biome06</th>
-            <td>'.$biome06.'</td>
-        </tr>
-    </tbody>
-</table>
-</div>
-'; 
-
-echo'
-<div class="col-sm-4">
-<p><strong>Soil-Spectral Info</strong></p>
-<table class="table table-secondary table-striped table-bordered table-sm">
-    <tbody>
-        <tr>
-            <th>Spectral01</th>
-            <td>'.$spectral01.'</td>
-        </tr>
-        <tr>
-            <th scope="row">Spectral02</th>
-            <td>'.$spectral02.'</td>
-        </tr>
-        <tr>
-            <th scope="row">Spectral03</th>
-            <td>'.$spectral03.'</td>
-        </tr>
-    </tbody>
-</table>
-</div></div>
-';
-
-?>
+'?>
