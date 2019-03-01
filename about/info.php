e9aab50db3521dc493c7b7e72428d280c6c56875
kileys-aafc <simon.kiley@canada.ca>
2019-03-01 14:07:47 -0500

    fixed content and layout

--- about/info.php
+++ about/info.php
@@ -1,15 +1,18 @@
-<head><title>About The System</title></head>
+<head><title>About the Archive</title></head>
 <?php include '../nav-template.php'; ?>
 <div class="container">
     <div class="row">
         <div class="col">
-            <h1 class="display-4 text-center">About this System</p>
+            <h1 class="display-4 text-center mb-4">About the Archive</p>
+            <hr class="mb-4">
         </div>
     </div>
+</div>
+<div class="container">
     <div class="row justify-content-center my-4">  
         <div class="col-8">
-            <p>The Soil Sample Management System is designed to organize the soil samples in storage room and manage sample data in designed database. Users can add new data, update existing sample data and query sample data.</p>
-            <p>The image below is a diagram of the designed storage room and corresponding storage hierarchy.</p>
+            <p>The Soil Sample Archive system is designed to organize the soil samples in the storage warehouse and manage sample data in a MySQL database. Users can query, add, and update soil sample data.</p>
+            <p>The image below is a diagram of the warehouse storage system.</p>
         </div>    
     </div>
     <div class="row justify-content-center">
