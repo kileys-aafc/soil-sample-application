e601976e3625a4ce2cfd818d82efeeb4c31c4047
kileys-aafc <simon.kiley@canada.ca>
2019-03-01 13:42:49 -0500

    updated content and added title

--- index.php
+++ index.php
@@ -1,43 +1,40 @@
 <?php include 'nav-template.php';?>    
-<head><link href="http://<?php echo $_SERVER['SERVER_NAME']; ?>/soil/connect/css/main-template.css" rel="stylesheet"></head>
 <main role="main" class="container">
-
-<section class="jumbotron text-center">
-	<div class="container">
-		<h1 class="jumbotron-heading">CanSIS Soil Samples</h1>
-		<p class="lead text-muted">This application allows you to query and manage the contents of the CanSIS Soil Sample Collection.</p>
-	</div>
-</section>
-
-<div class="card-deck">
-	<div class="card">    
-				<i class="fas fa-search bg-secondary text-center text-light py-4" style="font-size: 1000%"></i>
-	<div class="card-body">
-			<h5 class="card-title">Search Database</h5>
-			<p class="card-text">Query soil samples currently stored in the database.</p>
-			<p><a class="btn btn-secondary" href="/query/query-sample.php" role="button">View details</a></p>
+	<section class="jumbotron text-center">
+		<div class="container">
+			<h1 class="display-4"><strong>CanSIS Soil Sample Archive</strong></h1>
+			<div class="row justify-content-center">
+			<div class="col-md-7">
+				<p class="lead text-muted">This application allows you to query and manage the contents of the CanSIS Soil Sample Archive</p>
+			</div>
 		</div>
-	</div>
-	<div class="card">
-	<i class="fas fa-qrcode bg-secondary text-center text-light py-4" style="font-size: 1000%"></i>
+	</section>
+	<div class="card-deck">
+		<div class="card">    
+					<i class="fas fa-search bg-secondary text-center text-light py-4" style="font-size: 1000%"></i>
 		<div class="card-body">
-			<h5 class="card-title">Scan Samples</h5>
-			<p class="card-text">Scan and retrive sample data from warehouse.</p>
-			<p><a class="btn btn-secondary" href="/scan/scan-sample.php" role="button">View details</a></p>
+				<h5 class="card-title">Search Database</h5>
+				<p class="card-text">Query soil samples currently stored in the database</p>
+				<p><a class="btn btn-secondary" href="/query/query-sample.php" role="button">View details</a></p>
+			</div>
+		</div>
+		<div class="card">
+		<i class="fas fa-qrcode bg-secondary text-center text-light py-4" style="font-size: 1000%"></i>
+			<div class="card-body">
+				<h5 class="card-title">Scan Samples</h5>
+				<p class="card-text">Scan and retrieve sample data from the warehouse</p>
+				<p><a class="btn btn-secondary" href="/scan/scan-sample.php" role="button">View details</a></p>
+			</div>
+		</div>
+		<div class="card">
+		<i class="far fa-question-circle bg-secondary text-center text-light py-4" style="font-size: 1000%"></i>
+			<div class="card-body">
+				<h5 class="card-title">About the Archive</h5>
+				<p class="card-text">Information about the database and warehouse storage</p>
+				<p><a class="btn btn-secondary" href="/about/info.php" role="button">View details</a></p>
+			</div>
 		</div>
 	</div>
-	<div class="card">
-	<i class="far fa-question-circle bg-secondary text-center text-light py-4" style="font-size: 1000%"></i>
-		<div class="card-body">
-			<h5 class="card-title">About the System</h5>
-			<p class="card-text">Information about the database and warehouse storage.</p>
-			<p><a class="btn btn-secondary" href="/about/info.php" role="button">View details</a></p>
-		</div>
-	</div>
-</div>
-
-
-	 
 </main>
 </body>    
 </html>
