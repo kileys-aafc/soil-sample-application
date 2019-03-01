192cfe3b47989d74ad06859e1fbc771f3b0adde9
kileys-aafc <simon.kiley@canada.ca>
2019-03-01 14:19:03 -0500

    new content and fixed layout

--- about/info.php
+++ about/info.php
@@ -11,10 +11,17 @@
 <div class="container">
     <div class="row justify-content-center my-4">  
         <div class="col-8">
-            <p>The Soil Sample Archive system is designed to organize the soil samples in the storage warehouse and manage sample data in a MySQL database. Users can query, add, and update soil sample data.</p>
-            <p>The image below is a diagram of the warehouse storage system.</p>
+            <p>The Soil Sample Archive system is designed to organize soil samples in a storage warehouse and manage the associated sample data in a MySQL database. Users can query, add, and update soil sample data via this web application.</p>
+            <p>The image below is an entity relationship diagram of the data stored in the MySQL database.</p>
         </div>    
     </div>
+    <div class="row justify-content-center">
+        <div class="col-8">
+            <img class="img-fluid" src="../images/soil-archive-ER.png">
+            <p class="my-4">The image below is a diagram of the warehouse storage system.</p>
+        </div>
+    </div>
+    
     <div class="row justify-content-center">
         <div class="col-8">
             <img class="img-fluid" src="../images/storage-structure.png">
