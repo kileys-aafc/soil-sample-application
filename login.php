28eeac1765d11ab5ef96742b8a6f4a621707e4a9
kileys-aafc <simon.kiley@canada.ca>
2019-04-24 13:28:24 -0400

    changed login error messages to red text

--- login.php
+++ login.php
@@ -43,7 +43,7 @@
                                 $_SESSION['admin']=$admin;
                                 header("location:index.php");
                             }else{
-                                $password_err="The password you entered is not valid.";
+                                $password_err="The password you entered is not correct.";
                             }
                         }else{
                             echo "System error. Please contact the administrator.</br>";
@@ -95,7 +95,7 @@
       <input id="inputPassword" name= "password" class="form-control" placeholder="Password" required=""  value="<?php echo $password;?>" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;" autocomplete="off" type="password">
       
       <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      <p class="mt-0 mb-3 text-muted"><?php echo $username_err."</br>".$password_err;?></p>
+      <p class="mt-0 my-3 text-danger"><?php echo $username_err,$password_err;?></p>
       <p class="mt-3 mb-3 text-muted">For assistance <a href="mailto:simon.kiley@canada.ca?Subject=Soil%20Sample%20Application%20Assistance%20Required" target="_top">Contact Us</a></p>
     </form>
     
