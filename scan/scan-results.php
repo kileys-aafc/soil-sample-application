77826ee9f6453ce0cbf6ac37be1d2c6eae0c2961
kileys-aafc <simon.kiley@canada.ca>
2020-02-14 11:44:19 -0500

    removed jar 2 query

--- scan/scan-results.php
+++ scan/scan-results.php
@@ -445,62 +445,8 @@
                     else{
                         echo "<p>ERROR: No entries found. Please select a field</p>";
                     }
-
-                
-            $query_archive_jar_2 = "SELECT sample_info.sample_id , archive.* FROM sample_info left JOIN archive ON sample_info.sample_id = archive.sample_id where sample_info.sample_id = '$sample' and archive.jar = 2";
-            $archive_jar_2_response = @mysqli_query($dbc, $query_archive_jar_2);
-            if($archive_jar_2_response){
-                if(mysqli_num_rows($archive_jar_2_response)> 0){  
-                //--------------------RECORDS EXIST---------------    
-        
-                if ($archive_jar_2 = mysqli_fetch_array($archive_jar_2_response)){
-     
-                    echo'
-                    </div>
-                    <div class="row mt-3">
-                    <div class="col-sm-4">
-                    <h4><strong>Archive (Jar #2)</strong></h4>
-                    <table class="table table-secondary table-striped table-bordered table-sm">
-                        <tbody>
-                            <tr>
-                                <th>Sample ID</th>
-                                <td>'.$archive_jar_2['sample_id'].'</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Jar Number</th>
-                                <td>'.$archive_jar_2['jar'].'</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Year Archived</th>
-                                <td>'.$archive_jar_2['arch_year'].'</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Section</th>
-                                <td>'.$archive_jar_2['section'].'</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Column</th>
-                                <td>'.$archive_jar_2['arch_col'].'</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Row</th>
-                                <td>'.$archive_jar_2['arch_row'].'</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Box_id</th>
-                                <td>'.$archive_jar_2['box_id'].'</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    </div>
-                    ';
-                              
-                    mysqli_close($dbc);
-                }
-                else{
-                    echo "Error!";
-                }
-                    }}}
+          
+            }
 ?>
 </div>    
 </form>
